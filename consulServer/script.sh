3ddb658acf0d8acb26ea40ebe354718b4228e5bc
paul <themyth92@gmail.com>
2018-09-14 14:57:46 +0800

    Fix consul server script

--- consulServer/script.sh
+++ consulServer/script.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 sudo apt-get install unzip
 wget https://releases.hashicorp.com/consul/1.2.3/consul_1.2.3_linux_amd64.zip
 unzip consul_1.2.3_linux_amd64.zip
 mv consul /usr/local/bin
-consul agent -config-file ./config.json
+consul agent -config-file ./testConsul/consulServer/config.json
