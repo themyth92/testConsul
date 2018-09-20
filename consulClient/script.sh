3b3fdc6474aaf74d819016e353bee4a878cbf15a
paul <themyth92@gmail.com>
2018-09-20 11:46:51 +0800

    Fix nvm path

--- consulClient/script.sh
+++ consulClient/script.sh
@@ -9,7 +9,7 @@ mv consul /usr/local/bin
 # run the app first
 git clone https://github.com/themyth92/grpc-test
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-. ~/.nvm/nvm.sh
+. /.nvm/nvm.sh
 nvm install 10
 nvm use 10
 
